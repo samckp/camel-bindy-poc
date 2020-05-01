aab634ed2557ce59e6368a0380b1a9108d71e1ae
samckp <sam.mca.liet@gmail.com>
2020-05-01 09:09:02 +0530

    updated

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # camel-bindy-poc
+
+.setHeader("propCount", simple("${bean:incrementalService?method=addNumber(property.RowCount)}"))  //number Increment by given num
+
+Service Bean to Increment by given number.
--- src/main/java/com/bindy/poc/route/SimpleRoute.java
+++ src/main/java/com/bindy/poc/route/SimpleRoute.java
@@ -5,6 +5,7 @@ import org.apache.camel.LoggingLevel;
 import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.processor.aggregate.AggregationStrategy;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
@@ -12,19 +13,23 @@ import java.util.Map;
 
 @Component
 public class SimpleRoute  extends RouteBuilder {
+
+    @Value("${incrementBy}")
+    private int increaseByNum;
+
     @Override
     public void configure() throws Exception {
 
         from("{{inputPath}}")
                 .routeId("SimpleRoute")
                 .log("BODY  :: ${body}")
-                .setProperty("RowCount", (simple(String.valueOf(Integer.parseInt("100")))))
+                .setProperty("RowCount", (simple(String.valueOf(increaseByNum + Integer.parseInt("100")))))
 
                 .log(LoggingLevel.INFO, simple("${bean:incrementalService?method=addNumber(property.RowCount)}").toString())
-                .setHeader("propCount", simple("${bean:incrementalService?method=addNumber(property.RowCount)}"))  //number Increment by given num
+                .setProperty("propCount", simple("${bean:incrementalService?method=addNumber(property.RowCount)}"))  //number Increment by given num
 
                 .log(LoggingLevel.INFO, "headerRow :: ${header.headerRow} ")
-                .log(LoggingLevel.INFO, "RowCount :: ${property.RowCount}  ::  ${header.propCount} ")
+                .log(LoggingLevel.INFO, "RowCount :: ${property.RowCount}  ::  ${property.propCount} ")
         .to("mock:end") //{{outputPath}}")
         ;
     }
