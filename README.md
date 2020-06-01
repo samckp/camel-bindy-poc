056cffa8e748327b38134440716003e0f3b7a7bf
samckp <sam.mca.liet@gmail.com>
2020-06-01 07:34:45 +0530

    updated

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # camel-bindy-poc
 
+Read CSV using Bindy.
+
 .setHeader("propCount", simple("${bean:incrementalService?method=addNumber(property.RowCount)}"))  //number Increment by given num
 
 Service Bean to Increment by given number.
+
--- src/main/java/com/bindy/poc/route/SimpleRoute.java
+++ src/main/java/com/bindy/poc/route/SimpleRoute.java
@@ -22,14 +22,14 @@ public class SimpleRoute  extends RouteBuilder {
 
         from("{{inputPath}}")
                 .routeId("SimpleRoute")
-                .log("BODY  :: ${body}")
+                .log("BODY-->>  :: ${body}")
                 .setProperty("RowCount", (simple(String.valueOf(increaseByNum + Integer.parseInt("100")))))
 
                 .log(LoggingLevel.INFO, simple("${bean:incrementalService?method=addNumber(property.RowCount)}").toString())
                 .setProperty("propCount", simple("${bean:incrementalService?method=addNumber(property.RowCount)}"))  //number Increment by given num
 
-                .log(LoggingLevel.INFO, "headerRow :: ${header.headerRow} ")
-                .log(LoggingLevel.INFO, "RowCount :: ${property.RowCount}  ::  ${property.propCount} ")
+                .log(LoggingLevel.INFO, "headerRow--> :: ${header.headerRow} ")
+                .log(LoggingLevel.INFO, "RowCount--> :: ${property.RowCount}  ::  ${property.propCount} ")
         .to("mock:end") //{{outputPath}}")
         ;
     }
